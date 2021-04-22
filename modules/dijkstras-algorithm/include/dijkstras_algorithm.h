d21761061c95e34673552d5d78a215049864f812
RustamovAzer <43135865+RustamovAzer@users.noreply.github.com>
2021-04-22 19:40:11 +0300

    Update dijkstras_algorithm.h

--- modules/dijkstras-algorithm/include/dijkstras_algorithm.h
+++ modules/dijkstras-algorithm/include/dijkstras_algorithm.h
@@ -7,10 +7,6 @@
 
 using Matrix = std::vector<double>;
 
-Matrix RandomGraph(int verts, int edges, bool oriented = false);
-Matrix RandomGraphSparce(int verts, int edges, bool oriented = false);
-Matrix RandomGraphDence(int verts, int edges, bool oriented = false);
-
 Matrix dijkstras_algorithm(Matrix graph, int verts, int source_vertex);
 
 #endif  // MODULES_DIJKSTRAS_ALGORITHM_INCLUDE_DIJKSTRAS_ALGORITHM_H_
