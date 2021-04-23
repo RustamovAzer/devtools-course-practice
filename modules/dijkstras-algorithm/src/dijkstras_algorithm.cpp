8440cfc461b5b2ddcfae3cd5c918e9d710c42f0a
RustamovAzer <43135865+RustamovAzer@users.noreply.github.com>
2021-04-23 16:20:44 +0300

    Test coverage (#4)
    * add dijkstras algorithm
    
    * up
    
    * lint errors
    
    * Update dijkstras_algorithm.h
    
    * Update dijkstras_algorithm.cpp
    
    * remove iostream
    
    * Delete test_dijkstras_algorithm.cpp
    
    * Revert "Delete test_dijkstras_algorithm.cpp"
    
    This reverts commit b36f60a15f61d24faae9183f19ff1e34eaf9709b.
    
    * delete complex_number tests
    
    * get rid of <limits> and <algorithms>
    
    * Revert "delete complex_number tests"
    
    This reverts commit e9617d0c19ab44e7364baf6fb644a3ae5f78d8a9.
    
    * Update dijkstras_algorithm.cpp
    
    * delete dijkstras_algorithm
    
    * add .h
    
    * reintoduce dijkstras_algorithm
    
    * Update dijkstras_algorithm.cpp
    
    * remove "using Matrix "
    
    * lint changes
    
    * reverts + namespace
    
    * lint changes
    
    * lint changes 2
    
    * reduce scope of shortest_path
    
    * Update dijkstras_algorithm.cpp

---
+++ modules/dijkstras-algorithm/src/dijkstras_algorithm.cpp
@@ -0,0 +1,44 @@
+// Copyright 2021 Rustamov Azer
+
+#include <iostream>
+#include <limits>
+#include <vector>
+
+#include "include/dijkstras_algorithm.h"
+
+Matrix GraphAlgorithms::dijkstras_algorithm
+        (Matrix graph, int verts, int source_vertex) {
+    if (graph.size() != (unsigned)verts * verts) {
+        throw "Incorrect graph";
+    }
+    if (source_vertex >= verts) {
+        throw "Incorrecr source vertex";
+    }
+    double inf = std::numeric_limits<double>::infinity();
+    Matrix distance_to_verex(verts);
+    std::fill(distance_to_verex.begin(), distance_to_verex.end(), inf);
+    distance_to_verex[source_vertex] = 0.0;
+    int closest_vert_in, closest_vert_out;
+    while (true) {
+        double shortest_path;
+        shortest_path = inf;
+        for (int vert_in = 0; vert_in < verts; vert_in++) {
+            if (distance_to_verex[vert_in] != inf) {
+                for (int vert_out = 0; vert_out < verts; vert_out++) {
+                    if ((distance_to_verex[vert_out] == inf) &&
+                        (graph[vert_in * verts + vert_out] < shortest_path)) {
+                        closest_vert_in = vert_in;
+                        closest_vert_out = vert_out;
+                        shortest_path = graph[vert_in * verts + vert_out];
+                    }
+                }
+            }
+        }
+        if (shortest_path == inf)
+            break;
+        distance_to_verex[closest_vert_out] =
+            distance_to_verex[closest_vert_in] + shortest_path;
+    }
+
+    return distance_to_verex;
+}
