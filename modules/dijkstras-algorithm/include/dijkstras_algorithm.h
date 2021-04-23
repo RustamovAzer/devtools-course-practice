b86f227b646f10a279c2398fa172344d43aac5dc
RustamovAzer <43135865+RustamovAzer@users.noreply.github.com>
2021-04-23 13:55:01 +0300

    add .h

---
+++ modules/dijkstras-algorithm/include/dijkstras_algorithm.h
@@ -0,0 +1,12 @@
+// Copyright 2021 Rustamov Azer
+
+#include <vector>
+
+#ifndef MODULES_DIJKSTRAS_ALGORITHM_INCLUDE_DIJKSTRAS_ALGORITHM_H_
+#define MODULES_DIJKSTRAS_ALGORITHM_INCLUDE_DIJKSTRAS_ALGORITHM_H_
+
+using Matrix = std::vector<double>;
+
+Matrix dijkstras_algorithm(Matrix graph, int verts, int source_vertex);
+
+#endif  // MODULES_DIJKSTRAS_ALGORITHM_INCLUDE_DIJKSTRAS_ALGORITHM_H_
